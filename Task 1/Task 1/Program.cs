2aee829dd90603f478ba306a5a237ca1985fbd7d
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-10-20 22:22:09 +0300

    Add files via upload

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solving.Func func = Solving.Function;
+            Solving.DFunc dfunc = Solving.DFunction;
+            TableInfo(func, dfunc);
+
+            Console.ReadKey();
+        }
+
+        private static void TableInfo(Solving.Func f, Solving.DFunc df)
+        {
+            Console.WriteLine("Введите х мин");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите х макс");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг");
+            double step = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите эпсилон");
+            double eps = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите метод");
+            double choice = Convert.ToDouble(Console.ReadLine());
+
+            List<(double X1, double X2, double Root)> segments;
+            segments = new List<(double X1, double X2, double Root)>();
+
+            double root;
+            int count = 0;
+            Console.WriteLine("Уточняемый  |  Уточнённый  |  f(уточнённый)  |  Количество итераций");
+            if (choice == 0)
+            {
+                foreach (var item in segments)
+                {
+                    root = Solving.HalfDivision(f, item.X1, item.X2, eps, out count);
+                    Console.WriteLine("  {0}      |      {1}      |      {2}      |      {3}", item.Root, root, f(root), count);
+                }
+            }
+            if (choice == 1)
+            {
+                foreach (var item in segments)
+                {
+                    root = Solving.Tangent(f, df, item.X1, item.X2, eps, out count);
+                    Console.WriteLine("  {0}      |      {1}      |      {2}      |      {3}", item.Root, root, f(root), count);
+                }
+            }
+            if (choice == 2)
+            {
+                foreach (var item in segments)
+                {
+                    root = Solving.Chord(f, item.X1, item.X2, eps, out count);
+                    Console.WriteLine("  {0}      |      {1}      |      {2}      |      {3}", item.Root, root, f(root), count);
+                }
+            }
+        }
+    }
+}
---
+++ Task 1/Task 1/Solving.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class Solving
+    {
+        public delegate double Func(double x);
+        public delegate double DFunc(double x);
+
+        public static double Function(double x) => Math.Sin(x) + Math.Log(Math.Sin(x));
+        public static double DFunction(double x) => Math.Cos(x) + 1 / Math.Tan(x);
+
+        public static double HalfDivision(Func F, double x1, double x2, double eps, out int n)
+        {
+            n = 0;
+            double c = double.NaN;
+            while (F(x1) * F(x2) <= 0 && Math.Abs(x1 - x2) > eps)
+            {
+                n++;
+                if (F(x1) == 0) return x1;
+                if (F(x2) == 0) return x2;
+                c = (x1 + x2) / 2;
+                if (F(c) == 0) return c;
+                else
+                if (F(x1) * F(c) < 0) x2 = c;
+                else x1 = c;
+            }
+            return c;
+        }
+        public static double Tangent(Func F, DFunc DF, double x1, double x2, double eps, out int n)
+        {
+            n = 0;
+            while (Math.Abs(F(x1)) > eps && x1 < x2)
+            {
+                x1 -= F(x1) / DF(x1);
+                n++;
+            }
+            return x1;
+        }
+        public static double Chord(Func F, double x1, double x2, double eps, out int n)
+        {
+            n = 0;
+            while (Math.Abs(F(x1)) > eps)
+            {
+                x1 -= F(x1) * (x2 - x1) / (F(x2) - F(x1));
+                n++;
+            }
+            return x1;
+        }
+    }
+}
